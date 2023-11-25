4e9fe5ecce3533bb5d394a2ab253d4eed6c3fae1
ModSomJEED59 <modsomjeed59@ModSomJEED59s-MacBook-Air.local>
2023-11-25 14:45:12 +0700

    refactor function on off light

---
+++ code/src/command/lab/command/lightOutdoorOffCommand.ts
@@ -0,0 +1,11 @@
+import { Light } from "../devices";
+import { Command } from "./command";
+
+export class LightOutdoorOffCommand implements Command {
+
+    constructor(private light: Light) {}
+
+    public execute(): void {
+        this.light.off();
+    }
+}
--- code/src/command/lab/controller.test.ts
+++ code/src/command/lab/controller.test.ts
@@ -1,3 +1,26 @@
+import { LightOutdoorOffCommand } from "./command/lightOutdoorOffCommand";
+import { LightOutdoorOnCommand } from "./command/lightOutdoorOnCommand";
+import { Controller } from "./controller";
+import { Light } from "./devices";
+
 describe('[Command - lab] Controller', () => {
-  it('should execute device taht wrapped with command object', () => {});
+  it('should execute device that wrapped with command object', () => {
+    //given
+    const controller = new Controller();
+
+    const light = new Light();
+    light.on = jest.fn();
+    light.off = jest.fn();
+
+    //when
+    const lightOutdoorOnCommand = new LightOutdoorOnCommand(light);
+    const lightOutdoorOffCommand = new LightOutdoorOffCommand(light);
+
+    //then
+    controller.execute(lightOutdoorOnCommand);
+    expect(light.on).toBeCalledTimes(1);
+
+    controller.execute(lightOutdoorOffCommand);
+    expect(light.off).toBeCalledTimes(1);
+  });
 });
